bbbc6136ce7e843cbb00b44b1e1b1edb1bef7c1c
niialabi <taufiq.alabi@gmail.com>
2024-04-14 05:58:33 +0000

    main function added exec if main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -78,3 +78,20 @@ def get_db() -> connection.MySQLConnection:
                                       username=username,
                                       password=password,
                                       database=db_name)
+
+
+def main():
+    """ main function implementation """
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        line = ''
+        for key, value in row.items():
+            line += f'{key}={value}; '
+        logger.info(line)
+
+
+if __name__ == "__main__":
+    main()
