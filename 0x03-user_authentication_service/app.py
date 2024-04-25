c685491d1dbbae456e911c7cdc798b54fd04f1c7
niialabi <taufiq.alabi@gmail.com>
2024-04-25 23:22:54 +0000

    implemented create new user post

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 """ flask app imp. """
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/')
 def hello_main():
     """ jasonify dict """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """ endpoint to register users """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
