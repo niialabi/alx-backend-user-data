f7d1eb0b9dc58c835826a9541040af7d580fda3d
niialabi <taufiq.alabi@gmail.com>
2024-04-25 02:34:54 +0000

    pycodestyl fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,11 +4,11 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_main():
     """ jasonify dict """
     return jsonify({"message": "Bienvenue"})
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
