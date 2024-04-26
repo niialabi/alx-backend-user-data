fcb6b667dc74bcc1154668b4dcf574298048a4ae
niialabi <taufiq.alabi@gmail.com>
2024-04-26 04:07:44 +0000

    login method implemented

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ flask app imp. """
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 
 AUTH = Auth()
@@ -23,6 +23,17 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f'{email}', "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """ log in function """
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", AUTH.create_session(email))
+    return response
 
 
 if __name__ == "__main__":
