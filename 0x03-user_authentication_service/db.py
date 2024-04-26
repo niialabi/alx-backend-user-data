99a973d8ac65f9a135ec2220e99621a2b4fba3c7
niialabi <taufiq.alabi@gmail.com>
2024-04-26 04:58:39 +0000

    error fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,8 +35,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ save the user to the database.  """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        self.__session.add(user)
+        self.__session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
