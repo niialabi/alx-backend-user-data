db7f2da6e3124da5eb71d140c47154a681ef87c2
niialabi <taufiq.alabi@gmail.com>
2024-04-26 05:40:11 +0000

    get user from session id fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,6 +62,6 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-        return user
