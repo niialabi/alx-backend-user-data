9423fd196639d5b18e872907a65d2212fe8b6bca
niialabi <taufiq.alabi@gmail.com>
2024-04-26 04:26:12 +0000

    find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 import uuid
 
@@ -54,3 +55,13 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ Get user email from session_id if session is active """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except (NoResultFound, InvalidRequestError):
+            return None
+        return user
