e1fe36f8bb2a4b4814926d5052141c41a7613131
niialabi <taufiq.alabi@gmail.com>
2024-04-26 02:18:49 +0000

    type annon all func

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,20 +9,20 @@ AUTH = Auth()
 
 
 @app.route('/')
-def hello_main():
+def hello_main() -> str:
     """ jasonify dict """
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'])
-def users():
+def users() -> str:
     """ endpoint to register users """
     email, password = request.form.get("email"), request.form.get("password")
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": email, "message": "user created"})
+    return jsonify({"email": f'{email}', "message": "user created"})
 
 
 if __name__ == "__main__":
