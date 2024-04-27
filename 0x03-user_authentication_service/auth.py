f5bfaf41c293b4ab2aefc870d78ffd84494b0e38
niialabi <taufiq.alabi@gmail.com>
2024-04-27 17:52:40 +0000

    tweeked exception handling 2.0

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,7 +86,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError
+            raise ValueError()
         hashed_password = _hash_password(password)
         self._db.update_user(self.id, hashed_password=hashed_password,
                              reset_token=None)
